98286117f88bd4d50b0636a234d7f1bd152a977a
PetteriM1 <petteri.malkavaara1gmail.com>
2019-03-27 20:18:46 +0200

    Added %device_os% placeholder

--- src/main/java/scoretag/Main.java
+++ src/main/java/scoretag/Main.java
@@ -23,4 +23,37 @@ public class Main extends PluginBase {
             return "null";
         }
     }
+
+    public String getOS(Player p) {
+        switch(p.getLoginChainData().getDeviceOS()) {
+            case 1:
+                return "Android";
+            case 2:
+                return "iOS";
+            case 3:
+                return "Mac";
+            case 4:
+                return "Fire";
+            case 5:
+                return "Gear VR";
+            case 6:
+                return "HoloLens";
+            case 7:
+                return "Windows 10";
+            case 8:
+                return "Windows";
+            case 9:
+                return "Dedicated";
+            case 10:
+                return "tvOS";
+            case 11:
+                return "PlayStation";
+            case 12:
+                return "NX";
+            case 13:
+                return "Xbox";
+            default:
+                return "Unknown";
+        }
+    }
 }
