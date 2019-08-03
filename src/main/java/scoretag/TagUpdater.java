2348d0fdd308ab9f67c35d748127aac0395deb7c
Petteri <petteri.malkavaara1@gmail.com>
2019-08-03 18:46:34 +0300

    Don't set score tag if not changed

--- src/main/java/scoretag/TagUpdater.java
+++ src/main/java/scoretag/TagUpdater.java
@@ -16,7 +16,10 @@ public class TagUpdater extends Thread {
     @Override
     public void run() {
         for (Player p : plugin.getServer().getOnlinePlayers().values()) {
-            p.setScoreTag(PlaceholderAPI.getInstance().translateString(plugin.config.getString("tag").replace("%factions_name%", plugin.getFaction(p)).replace("%device_os%", plugin.getOS(p)), p));
+            String tag = PlaceholderAPI.getInstance().translateString(plugin.config.getString("tag").replace("%factions_name%", plugin.getFaction(p)).replace("%device_os%", plugin.getOS(p)), p);
+            if (!p.getScoreTag().equals(tag)) {
+                p.setScoreTag(tag);
+            }
         }
     }
 }
