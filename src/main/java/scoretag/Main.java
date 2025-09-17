5e5a0226918c5d14a62e1f7d861da44350ec8db8
Petteri <26197131+PetteriM1@users.noreply.github.com>
2025-09-17 17:02:35 +0300

    Actually MorePlaceholders already provide

--- src/main/java/scoretag/Main.java
+++ src/main/java/scoretag/Main.java
@@ -1,6 +1,5 @@
 package scoretag;
 
-import cn.nukkit.Player;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.utils.Config;
 
@@ -8,7 +7,6 @@ public class Main extends PluginBase {
 
     String tag;
     boolean useOs;
-    boolean useFaction;
     boolean useTotalHealth;
 
     @Override
@@ -22,7 +20,6 @@ public class Main extends PluginBase {
         Config config = getConfig();
         tag = config.getString("tag");
         useOs = tag.contains("%device_os%");
-        useFaction = tag.contains("%factions_name%");
         useTotalHealth = tag.contains("%total_health%");
 
         if (getServer().getPluginManager().getPlugin("PlaceholderAPI") == null) {
@@ -32,14 +29,5 @@ public class Main extends PluginBase {
         }
 
         getServer().getScheduler().scheduleDelayedRepeatingTask(this, new TagUpdater(this), config.getInt("update"), config.getInt("update"));
-    }
-
-    static String getFaction(Player p) {
-        try {
-            Class.forName("com.massivecraft.factions.P");
-            return com.massivecraft.factions.P.p.getPlayerFactionTag(p);
-        } catch (Exception e) {
-            return "null";
-        }
     }
 }
--- src/main/java/scoretag/TagUpdater.java
+++ src/main/java/scoretag/TagUpdater.java
@@ -18,12 +18,11 @@ public class TagUpdater implements Runnable {
     public void run() {
         for (Player p : plugin.getServer().getOnlinePlayers().values()) {
             String str = plugin.tag;
+
             if (plugin.useOs) {
                 str = str.replace("%device_os%", Utils.getOS(p));
             }
-            if (plugin.useFaction) {
-                str = str.replace("%factions_name%", Main.getFaction(p));
-            }
+
             if (plugin.useTotalHealth) {
                 str = str.replace("%total_health%", String.format(Locale.US, "%.1f", p.getHealth() + p.getAbsorption()));
             }
